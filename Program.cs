a19da9191647563a2c145d0531e42d121d186355
Tyler Ramage <tysamigo@gmail.com>
2020-07-16 17:31:28 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace project_csharp_soccer_stats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
