663c6c355cb7da94b0ccd9b25d3030421388b7cd
Tyler Ramage <tysamigo@gmail.com>
2020-07-18 21:08:28 -0400

    add StreamReader

--- Program.cs
+++ Program.cs
@@ -10,12 +10,7 @@ namespace project_csharp_soccer_stats
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
             var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
-            var fileContents = ReadFile(fileName);
-            string[] fileLines = fileContents.Split(new char[] { '\r', '\n'});
-            foreach(var line in fileLines)
-            {
-                Console.WriteLine(line);
-            }
+            var fileContents = ReadSoccerResults(fileName);
             }
 
         public static string ReadFile(string fileName)
@@ -24,6 +19,21 @@ namespace project_csharp_soccer_stats
             {
                 return reader.ReadToEnd();
             }
+        }
+
+        public static List<string[]> ReadSoccerResults(string fileName)
+        {
+            var soccerResults = new List<string[]>();
+            using(var reader = new StreamReader(fileName))
+            {
+                string line = "";
+                while((line = reader.ReadLine()) !=null)
+                {
+                    string[] values = line.Split(',');
+                    soccerResults.Add(values);
+                }
+            }
+            return soccerResults;
         }
     }
 }
